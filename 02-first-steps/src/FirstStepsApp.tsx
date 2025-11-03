ef94642a02749ab9e193b67d7097f01fabbd93e1
dieginin <diegobalestra9@gmail.com>
2025-11-03 00:37:25 -0800

    feat: add FirstStepsApp and MyAwesomeApp components, update main.tsx to render them

---
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -0,0 +1,14 @@
+export function FirstStepsApp() {
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <p>Esto es un párrafo</p>
+
+      <button>Click Me</button>
+
+      <div>
+        <h2>Hola dentro del div</h2>
+      </div>
+    </>
+  )
+}
---
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -0,0 +1,8 @@
+export function MyAwesomeApp() {
+  return (
+    <>
+      <h1>Diego</h1>
+      <h3>Balestra</h3>
+    </>
+  )
+}
