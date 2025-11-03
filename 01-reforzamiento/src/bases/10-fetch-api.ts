6a20f8ce4c979eae23c124e2c4a2c1a1a86826d7
dieginin <diegobalestra9@gmail.com>
2025-11-02 23:04:30 -0800

    feat: refactor Giphy API fetch logic and add GiphyResponse interface

--- 01-reforzamiento/src/bases/10-fetch-api.ts
+++ 01-reforzamiento/src/bases/10-fetch-api.ts
@@ -1,17 +1,20 @@
+import type { GiphyResponse } from "../data/giphy.response"
+
 const API_KEY = "38lZjFNPNKurPr7sVvcIDOcMxZM7GeLJ"
 
 const myRequest = fetch(
   `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
 )
 
+const displayGiphyImage = (url: string) => {
+  const imageElement = document.createElement("img")
+  imageElement.src = url
+  document.body.append(imageElement)
+}
+
 myRequest
   .then((response) => response.json())
-  .then(({ data }) => {
-    const imageUrl = data.images.original.url
-
-    const imageElement = document.createElement("img")
-    imageElement.src = imageUrl
-
-    document.body.append(imageElement)
-  })
+  .then(({ data }: GiphyResponse) =>
+    displayGiphyImage(data.images.original.url)
+  )
   .catch((error) => console.error(error))
