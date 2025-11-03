72c877516596b02e5051dfde51e2e9029f4edbc4
dieginin <diegobalestra9@gmail.com>
2025-11-02 19:28:31 -0800

    feat: refactor getUser function to include User interface and update getUser2 to arrow function

--- 01-reforzamiento/src/bases/05-functions.ts
+++ 01-reforzamiento/src/bases/05-functions.ts
@@ -9,21 +9,28 @@ const message2 = greet2("Vegeta")
 console.log(message)
 console.log(message2)
 
-function getUser() {
+interface User {
+  uid: string
+  username: string
+}
+
+function getUser(): User {
   return {
     uid: "17775298-f406-5c08-a8c4-cc1611dbd6fb",
     username: "jonsfg4",
   }
 }
-const getUser2 = () => {
-  return {
-    uid: "17775298-f406-5c08-a8c4-cc1611dbd6fb",
-    username: "jonsfg4",
-  }
-}
+const getUser2 = (): User => ({
+  uid: "17775298-f406-5c08-a8c4-cc1611dbd6fb",
+  username: "jonsfg4",
+})
 
 const user = getUser()
 const user2 = getUser2()
 
 console.log(user)
 console.log(user2)
+
+const myNumbers: number[] = [1, 2, 3, 4, 5]
+
+myNumbers.forEach(console.log)
