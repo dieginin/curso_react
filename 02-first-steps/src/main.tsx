ed6180d2657d2bd59e9e812bce07cd88060c8be8
dieginin <diegobalestra9@gmail.com>
2025-11-03 00:16:55 -0800

    feat: remove unused CSS and App component, simplify main.tsx

--- 02-first-steps/src/main.tsx
+++ 02-first-steps/src/main.tsx
@@ -1,11 +1,8 @@
-import "./index.css"
-
-import App from "./App.tsx"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <h1>Hola Mundo</h1>
   </StrictMode>
 )
