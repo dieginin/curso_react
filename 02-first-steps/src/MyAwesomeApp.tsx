a96f58512d6fb890fd9ed2c5c55d6853e121ce09
dieginin <diegobalestra9@gmail.com>
2025-11-03 21:24:12 -0800

    feat: refactor MyAwesomeApp component to use constants for rendering

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,8 +1,26 @@
+const firstName = "Diego"
+const lastName = "Balestra"
+
+const favoriteGames = ["Brawl Stars", "Smash", "Mario Kart"]
+
+const isActive = true
+
+const address = {
+  zipCode: "ABC123",
+  country: "Canada",
+}
+
 export function MyAwesomeApp() {
   return (
     <>
-      <h1>Diego</h1>
-      <h3>Balestra</h3>
+      <h1>{firstName}</h1>
+      <h3>{lastName}</h3>
+
+      <p>{favoriteGames.join(", ")}</p>
+
+      <h1>{isActive ? "Activo" : "No Activo"}</h1>
+
+      <p>{JSON.stringify(address)}</p>
     </>
   )
 }
