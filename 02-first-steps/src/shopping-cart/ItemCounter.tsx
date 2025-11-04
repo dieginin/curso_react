cdc1de7daf707fc4bbb3efedfdb5bee0b37ba068
dieginin <diegobalestra9@gmail.com>
2025-11-03 22:35:44 -0800

    feat: update ItemCounter component to manage internal state for item count and handle increment/decrement actions

--- 02-first-steps/src/shopping-cart/ItemCounter.tsx
+++ 02-first-steps/src/shopping-cart/ItemCounter.tsx
@@ -1,19 +1,26 @@
+import { useState } from "react"
+
 interface Props {
   name: string
   quantity: number
 }
 
 export default function ItemCounter({ name, quantity }: Props) {
+  const [count, setCount] = useState(quantity)
+
+  const handleDecrement = () => setCount(Math.max(0, count - 1))
+  const handleIncrement = () => setCount(count + 1)
+
   return (
     <section
       style={{ display: "flex", alignItems: "center", gap: 10, marginTop: 10 }}
     >
       <span style={{ width: 150 }}>{name}</span>
-      <button>
+      <button onClick={handleDecrement}>
         <small>-1</small>
       </button>
-      <span>{quantity}</span>
-      <button>
+      <span>{count}</span>
+      <button onClick={handleIncrement}>
         <small>+1</small>
       </button>
     </section>
