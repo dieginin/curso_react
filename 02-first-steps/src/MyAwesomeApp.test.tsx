bd9db744f474a0df39db39defa67aecabb764717
dieginin <diegobalestra9@gmail.com>
2025-11-05 23:59:50 -0800

    feat: enhance MyAwesomeApp tests with data-testid for first name

--- 02-first-steps/src/MyAwesomeApp.test.tsx
+++ 02-first-steps/src/MyAwesomeApp.test.tsx
@@ -1,12 +1,26 @@
-import { describe, test } from "vitest"
+import { describe, expect, test } from "vitest"
 import { render, screen } from "@testing-library/react"
 
 import { MyAwesomeApp } from "./MyAwesomeApp"
 
 describe("MyAwesomeApp", () => {
   test("should render firstName and LastName", () => {
+    // CON CONTAINER
+    const { container } = render(<MyAwesomeApp />)
+
+    const h1 = container.querySelector("h1")
+    const h3 = container.querySelector("h3")
+
+    expect(h1?.innerHTML).toContain("Diego")
+    expect(h3?.innerHTML).toContain("Balestra")
+  })
+
+  test("should render firstName and LastName", () => {
+    // CON SCREEN
     render(<MyAwesomeApp />)
 
-    screen.debug()
+    const h1 = screen.getByTestId("first-name-title")
+
+    expect(h1.innerHTML).toBe("Diego")
   })
 })
