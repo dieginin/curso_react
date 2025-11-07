acbcc2b0b8a1e57aec107899a94cde7f7fa1b383
dieginin <diegobalestra9@gmail.com>
2025-11-06 22:28:10 -0800

    feat: add tests for increment and decrement functionality in ItemCounter component

--- 02-first-steps/src/shopping-cart/ItemCounter.test.tsx
+++ 02-first-steps/src/shopping-cart/ItemCounter.test.tsx
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "vitest"
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 
 import ItemCounter from "./ItemCounter"
 
@@ -20,5 +20,35 @@ describe("ItemCounter", () => {
     render(<ItemCounter name={itemName} quantity={itemQuantity} />)
 
     expect(screen.getByText(itemQuantity)).toBeDefined()
+  })
+
+  test("should increase count when +1 button is pressed", () => {
+    render(<ItemCounter name={"Test Item"} />)
+
+    const [, buttonAdd] = screen.getAllByRole("button")
+
+    fireEvent.click(buttonAdd)
+
+    expect(screen.getAllByText(2)).toBeDefined()
+  })
+
+  test("should decrease count when -1 button is pressed", () => {
+    render(<ItemCounter name={"Test Item"} />)
+
+    const [buttonDecrease] = screen.getAllByRole("button")
+
+    fireEvent.click(buttonDecrease)
+
+    expect(screen.getAllByText(0)).toBeDefined()
+  })
+
+  test("should not decrease count when -1 button is pressed and quantity is 0", () => {
+    render(<ItemCounter name={"Test Item"} quantity={0} />)
+
+    const [buttonDecrease] = screen.getAllByRole("button")
+
+    fireEvent.click(buttonDecrease)
+
+    expect(screen.getAllByText(0)).toBeDefined()
   })
 })
