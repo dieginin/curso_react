d050542a245854b2613a48f58341c7fb2c4dc863
dieginin <diegobalestra9@gmail.com>
2025-11-08 20:34:29 -0800

    feat: implement GifsApp component with search functionality and mock data

--- 03-gifs-app/src/main.tsx
+++ 03-gifs-app/src/main.tsx
@@ -1,10 +1,11 @@
 import "./index.css"
 
+import { GifsApp } from "./GifsApp"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>Hola Mundo</h1>
+    <GifsApp />
   </StrictMode>
 )
