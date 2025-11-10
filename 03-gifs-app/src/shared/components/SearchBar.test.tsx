bc620f42364ea22950e54a11a40966660afef0b6
dieginin <diegobalestra9@gmail.com>
2025-11-09 21:00:22 -0800

    test: add debounce functionality and placeholder verification for SearchBar component

--- 03-gifs-app/src/shared/components/SearchBar.test.tsx
+++ 03-gifs-app/src/shared/components/SearchBar.test.tsx
@@ -12,11 +12,9 @@ describe("SearchBar", () => {
 
   test("should call onSearch with the correct value after 700ms", async () => {
     const onSearch = vi.fn()
-
     render(<SearchBar onSearch={onSearch} />)
 
     const input = screen.getByRole("textbox")
-
     fireEvent.change(input, { target: { value: "test" } })
 
     expect(onSearch).not.toBeCalled()
@@ -24,5 +22,41 @@ describe("SearchBar", () => {
       expect(onSearch).toBeCalled()
       expect(onSearch).toBeCalledWith("test")
     })
+  })
+
+  test("should call only once with the las value (debounce)", async () => {
+    const onSearch = vi.fn()
+    render(<SearchBar onSearch={onSearch} />)
+
+    const input = screen.getByRole("textbox")
+    fireEvent.change(input, { target: { value: "t" } })
+    fireEvent.change(input, { target: { value: "te" } })
+    fireEvent.change(input, { target: { value: "tes" } })
+    fireEvent.change(input, { target: { value: "test" } })
+
+    await waitFor(() => {
+      expect(onSearch).toBeCalledTimes(1)
+      expect(onSearch).toBeCalledWith("test")
+    })
+  })
+
+  test("should call onSearch en button click with input value", () => {
+    const onSearch = vi.fn()
+    render(<SearchBar onSearch={onSearch} />)
+
+    const input = screen.getByRole("textbox")
+    fireEvent.change(input, { target: { value: "test" } })
+
+    const button = screen.getByRole("button")
+    fireEvent.click(button)
+
+    expect(onSearch).toBeCalledTimes(1)
+    expect(onSearch).toBeCalledWith("test")
+  })
+
+  test("should input have the correct placeholder value", () => {
+    render(<SearchBar placeholder='Search Test' onSearch={() => {}} />)
+
+    expect(screen.getByPlaceholderText("Search Test")).toBeDefined()
   })
 })
