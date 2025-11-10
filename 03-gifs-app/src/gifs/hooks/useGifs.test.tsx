806919b67161ccf6d03f89b202afd8018b5720f6
dieginin <diegobalestra9@gmail.com>
2025-11-09 20:22:22 -0800

    test: enhance useGifs tests for caching and previous searches limit

--- 03-gifs-app/src/gifs/hooks/useGifs.test.tsx
+++ 03-gifs-app/src/gifs/hooks/useGifs.test.tsx
@@ -1,5 +1,7 @@
+import * as gifActions from "../services/get-gifs-by-query.action"
+
 import { act, renderHook } from "@testing-library/react"
-import { describe, expect, test } from "vitest"
+import { describe, expect, test, vi } from "vitest"
 
 import { useGifs } from "./useGifs"
 
@@ -31,5 +33,70 @@ describe("useGifs", () => {
     })
 
     expect(result.current.gifs.length).toBe(10)
+  })
+
+  test("should return a list of gifs from cache", async () => {
+    const { result } = renderHook(() => useGifs())
+
+    await act(async () => {
+      await result.current.handleSearchClicked("Test")
+    })
+
+    expect(result.current.gifs.length).toBe(10)
+
+    vi.spyOn(gifActions, "GetGifsByQuery").mockRejectedValue(
+      new Error("Custom Error")
+    )
+
+    await act(async () => {
+      await result.current.handleSearchClicked("Test")
+    })
+    expect(result.current.gifs.length).toBe(10)
+  })
+
+  test("should return no more than 8 previous searches", async () => {
+    const { result } = renderHook(() => useGifs())
+
+    vi.spyOn(gifActions, "GetGifsByQuery").mockResolvedValue([])
+
+    await act(async () => {
+      await result.current.handleSearch("Test1")
+    })
+    await act(async () => {
+      await result.current.handleSearch("Test2")
+    })
+    await act(async () => {
+      await result.current.handleSearch("Test3")
+    })
+    await act(async () => {
+      await result.current.handleSearch("Test4")
+    })
+    await act(async () => {
+      await result.current.handleSearch("Test5")
+    })
+    await act(async () => {
+      await result.current.handleSearch("Test6")
+    })
+    await act(async () => {
+      await result.current.handleSearch("Test7")
+    })
+    await act(async () => {
+      await result.current.handleSearch("Test8")
+    })
+    await act(async () => {
+      await result.current.handleSearch("Test9")
+    })
+
+    expect(result.current.previousSearches.length).toBe(8)
+    expect(result.current.previousSearches).toStrictEqual([
+      "test9",
+      "test8",
+      "test7",
+      "test6",
+      "test5",
+      "test4",
+      "test3",
+      "test2",
+    ])
   })
 })
