a159f356801bc5d37823bb81060935106134acd2
dieginin <diegobalestra9@gmail.com>
2025-11-09 19:16:24 -0800

    test: add unit tests for useGifs hook functionality

---
+++ 03-gifs-app/src/gifs/hooks/useGifs.test.tsx
@@ -0,0 +1,35 @@
+import { act, renderHook } from "@testing-library/react"
+import { describe, expect, test } from "vitest"
+
+import { useGifs } from "./useGifs"
+
+describe("useGifs", () => {
+  test("should return default values and methods", () => {
+    const { result } = renderHook(() => useGifs())
+
+    expect(result.current.gifs.length).toBe(0)
+    expect(result.current.previousSearches.length).toBe(0)
+    expect(result.current.handleSearch).toBeDefined()
+    expect(result.current.handleSearchClicked).toBeDefined()
+  })
+
+  test("should return a list of gifs", async () => {
+    const { result } = renderHook(() => useGifs())
+
+    await act(async () => {
+      await result.current.handleSearch("Test")
+    })
+
+    expect(result.current.gifs.length).toBe(10)
+  })
+
+  test("should return a list of gifs when handleSearchClicked is called", async () => {
+    const { result } = renderHook(() => useGifs())
+
+    await act(async () => {
+      await result.current.handleSearchClicked("Test")
+    })
+
+    expect(result.current.gifs.length).toBe(10)
+  })
+})
