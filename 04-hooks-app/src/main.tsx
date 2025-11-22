e96c89ec478ecfbcefc29f2df463d51683131c54
dieginin <diegobalestra9@gmail.com>
2025-11-22 11:37:23 -0800

    feat: implement task management app with useReducer
    - Added TaskApp component for managing tasks with add, toggle, and delete functionalities.
    - Introduced UI components: Button, Card, Checkbox, and Input for better UI structure.
    - Updated package.json to include new dependencies for UI components and utilities.
    - Enhanced styling with Tailwind CSS and custom CSS variables for theming.
    - Configured TypeScript paths for easier imports.
    - Updated Vite configuration to support path aliasing.

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,12 +1,13 @@
 import "./index.css"
 
-import FocusScreen from "./04-useRef/FocusScreen.tsx"
 // import { HooksApp } from "./HooksApp.tsx"
 // import { TrafficLight } from "./01-useState/TrafficLight.tsx"
 // import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect.tsx"
 // import { TrafficLightWithEffectWithHook } from "./02-useEffect/TrafficLightWithEffectWithHook.tsx"
 // import { PokemonPage } from "./03-examples/PokemonPage.tsx"
+// import FocusScreen from "./04-useRef/FocusScreen.tsx"
 import { StrictMode } from "react"
+import { TasksApp } from "./05-useReducer/TaskApp.tsx"
 import { createRoot } from "react-dom/client"
 
 createRoot(document.getElementById("root")!).render(
@@ -16,6 +17,7 @@ createRoot(document.getElementById("root")!).render(
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithEffectWithHook /> */}
     {/* <PokemonPage /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    <TasksApp />
   </StrictMode>
 )
