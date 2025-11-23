40e58f5ffe6b572a025bf3f6d12c2b603ef621bd
dieginin <diegobalestra9@gmail.com>
2025-11-22 22:21:09 -0800

    feat: implement useTransition for comment submission in InstagromApp

--- 04-hooks-app/src/07-useOptimistic/InstagromApp.tsx
+++ 04-hooks-app/src/07-useOptimistic/InstagromApp.tsx
@@ -1,4 +1,4 @@
-import { useOptimistic, useState } from "react"
+import { useOptimistic, useState, useTransition } from "react"
 
 interface Comment {
   id: number
@@ -6,7 +6,11 @@ interface Comment {
   optimistic?: boolean
 }
 
+let lastId = 2
+
 export const InstagromApp = () => {
+  const [isPending, startTransition] = useTransition()
+
   const [comments, setComments] = useState<Comment[]>([
     { id: 1, text: "¡Gran foto!" },
     { id: 2, text: "Me encanta 🧡" },
@@ -14,25 +18,33 @@ export const InstagromApp = () => {
 
   const [optimisticComments, addOptimisticComment] = useOptimistic(
     comments,
-    (currentComments, newCommentText: string) => [
-      ...currentComments,
-      {
-        id: new Date().getTime(),
-        text: newCommentText,
-        optimistic: true,
-      },
-    ]
+    (currentComments, newCommentText: string) => {
+      lastId++
+      return [
+        ...currentComments,
+        {
+          id: lastId,
+          text: newCommentText,
+          optimistic: true,
+        },
+      ]
+    }
   )
 
-  const handleAddComment = async (formData: FormData) => {
+  const handleAddComment = (formData: FormData) => {
     const messageText = formData.get("post-message") as string
 
     addOptimisticComment(messageText)
 
-    // Simular la petición HTTP al servidor
-    await new Promise((resolve) => setTimeout(resolve, 3000))
+    startTransition(async () => {
+      // Simular la petición HTTP al servidor
+      await new Promise((resolve) => setTimeout(resolve, 3000))
 
-    setComments([...comments, { id: new Date().getTime(), text: messageText }])
+      setComments([
+        ...comments,
+        { id: new Date().getTime(), text: messageText },
+      ])
+    })
   }
 
   return (
@@ -78,7 +90,7 @@ export const InstagromApp = () => {
         />
         <button
           type='submit'
-          disabled={false}
+          disabled={isPending}
           className='bg-blue-500 text-white p-2 rounded-md w-full'
         >
           Enviar
