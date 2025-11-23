c925d1f8d7f13e15be8f6f1c8af7951fdbf5547e
dieginin <diegobalestra9@gmail.com>
2025-11-22 20:40:12 -0800

    feat: add API call functionality to MySubtitle component and optimize MemoHook

--- 04-hooks-app/src/06-memos/MemoHook.tsx
+++ 04-hooks-app/src/06-memos/MemoHook.tsx
@@ -1,17 +1,23 @@
+import { useCallback, useState } from "react"
+
 import { MySubtitle } from "./ui/MySubtitle"
 import { MyTitle } from "./ui/MyTitle"
-import { useState } from "react"
 
 export default function MemoHook() {
   const [title, setTitle] = useState("Mi Titulo")
   const [subtitle, setSubTitle] = useState("Mi Subtitulo")
+
+  const handleApiCall = useCallback(
+    () => console.log("llamar a mi API - ", subtitle),
+    [subtitle]
+  )
 
   return (
     <div className='bg-gradient flex flex-col gap-4'>
       <h1 className='text-2xl font-thin text-white'>MemoHook</h1>
 
       <MyTitle title={title} />
-      <MySubtitle subtitle={subtitle} />
+      <MySubtitle subtitle={subtitle} callMyApi={handleApiCall} />
 
       <button
         className='bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer'
--- 04-hooks-app/src/06-memos/ui/MySubtitle.tsx
+++ 04-hooks-app/src/06-memos/ui/MySubtitle.tsx
@@ -2,16 +2,20 @@ import { memo } from "react"
 
 interface Props {
   subtitle: string
+  callMyApi: () => void
 }
 
-export const MySubtitle = memo(({ subtitle }: Props) => {
+export const MySubtitle = memo(({ subtitle, callMyApi }: Props) => {
   console.log("MySubtitle re-render")
 
   return (
     <>
       <h6 className='text-2xl font-bold'>{subtitle}</h6>
 
-      <button className='bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer'>
+      <button
+        className='bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer'
+        onClick={callMyApi}
+      >
         Llamar a funcion
       </button>
     </>
