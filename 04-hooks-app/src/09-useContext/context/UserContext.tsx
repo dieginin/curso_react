1a921a552e4e1d1dc16892ef6072990a8b2a0cb7
dieginin <diegobalestra9@gmail.com>
2025-11-23 23:53:29 -0800

    feat: implement UserContext with authentication state management and user data structure

--- 04-hooks-app/src/09-useContext/context/UserContext.tsx
+++ 04-hooks-app/src/09-useContext/context/UserContext.tsx
@@ -1,5 +1,33 @@
-import type { PropsWithChildren } from "react"
+import { createContext, useState, type PropsWithChildren } from "react"
+import type { User } from "../data/user-mock.data"
+
+type AuthStatusType = "checking" | "authenticated" | "non-authenticated"
+
+interface UserContextProps {
+  //! State
+  authStatus: AuthStatusType
+  user: User | null
+
+  //! Methods
+  login: (userId: number) => boolean
+  logout: () => void
+}
+
+const UserContext = createContext({} as UserContextProps)
 
 export const UserContextProvider = ({ children }: PropsWithChildren) => {
-  return children
+  const [authStatus, setAuthStatus] = useState<AuthStatusType>("checking")
+  const [user, setUser] = useState<User | null>(null)
+
+  const handleLogin = (id: number) => id === id
+  const handleLogout = () => {}
+
+  const userContextValue = {
+    authStatus: authStatus,
+    user: user,
+    login: handleLogin,
+    logout: handleLogout,
+  }
+
+  return <UserContext value={userContextValue}>{children}</UserContext>
 }
