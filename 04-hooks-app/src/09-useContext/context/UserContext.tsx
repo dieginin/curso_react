b045f751b798e9e9f009a53ca17ee1ecbae330b3
dieginin <diegobalestra9@gmail.com>
2025-11-24 09:39:20 -0800

    feat: enhance UserContext with localStorage integration for user authentication and update ProfilePage to include logout functionality

--- 04-hooks-app/src/09-useContext/context/UserContext.tsx
+++ 04-hooks-app/src/09-useContext/context/UserContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, useState, type PropsWithChildren } from "react"
+import {
+  createContext,
+  useEffect,
+  useState,
+  type PropsWithChildren,
+} from "react"
 import { users, type User } from "../data/user-mock.data"
 
 type AuthStatusType = "checking" | "authenticated" | "non-authenticated"
@@ -19,23 +24,32 @@ function UserContextProvider({ children }: PropsWithChildren) {
   const [authStatus, setAuthStatus] = useState<AuthStatusType>("checking")
   const [user, setUser] = useState<User | null>(null)
 
-  const handleLogin = (id: number) => {
-    const user = users.find((user) => user.id === id)
+  const handleLogin = (userId: number) => {
+    const user = users.find((user) => user.id === userId)
 
     if (!user) {
-      console.error(`User not found ${id}`)
+      console.error(`User not found ${userId}`)
       setUser(null)
       setAuthStatus("non-authenticated")
       return false
     }
     setUser(user)
     setAuthStatus("authenticated")
+    localStorage.setItem("userId", userId.toString())
     return true
   }
   const handleLogout = () => {
     setUser(null)
     setAuthStatus("non-authenticated")
+    localStorage.removeItem("userId")
   }
+
+  useEffect(() => {
+    const storedUserId = localStorage.getItem("userId")
+
+    if (storedUserId) handleLogin(+storedUserId)
+    handleLogout()
+  }, [])
 
   const userContextValue = {
     authStatus: authStatus,
