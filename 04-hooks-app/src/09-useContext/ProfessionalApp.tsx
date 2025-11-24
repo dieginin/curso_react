b86396f9a9ecb8e18030989dd7aacdbdd47443e6
dieginin <diegobalestra9@gmail.com>
2025-11-23 22:50:29 -0800

    feat: add react-router for navigation and implement appRouter

--- 04-hooks-app/src/09-useContext/ProfessionalApp.tsx
+++ 04-hooks-app/src/09-useContext/ProfessionalApp.tsx
@@ -1,7 +1,10 @@
+import { RouterProvider } from "react-router/dom"
+import { appRouter } from "./router/app.router"
+
 export const ProfessionalApp = () => {
   return (
     <div className='bg-gradient flex flex-col'>
-      <h1>Professional App</h1>
+      <RouterProvider router={appRouter} />
     </div>
   )
 }
