f0ecf4ad1c546a035efd78f769739c3ea70230b2
dieginin <diegobalestra9@gmail.com>
2025-11-24 17:44:33 -0800

    feat: implement CustomPagination component and integrate into HomePage for improved navigation

--- 05-heroes-app/src/heroes/pages/home/HomePage.tsx
+++ 05-heroes-app/src/heroes/pages/home/HomePage.tsx
@@ -1,8 +1,7 @@
-import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
-import { Button } from "@/components/ui/button"
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
+import { CustomPagination } from "@/components/custom/CustomPagination"
 import { HeroGrid } from "@/heroes/components/HeroGrid"
 import { HeroStats } from "@/heroes/components/HeroStats"
 import { useState } from "react"
@@ -50,14 +49,17 @@ export const HomePage = () => {
           {/* Display all characters */}
           <HeroGrid />
         </TabsContent>
+
         <TabsContent value='favorites'>
           {/* Display favorite characters */}
           <HeroGrid />
         </TabsContent>
+
         <TabsContent value='heroes'>
           {/* Display all heroes */}
           <HeroGrid />
         </TabsContent>
+
         <TabsContent value='villains'>
           {/* Display all villains */}
           <HeroGrid />
@@ -65,30 +67,7 @@ export const HomePage = () => {
       </Tabs>
 
       {/* Pagination */}
-      <div className='flex items-center justify-center space-x-2'>
-        <Button variant='outline' size='sm' disabled>
-          <ChevronLeft className='h-4 w-4' />
-          Previous
-        </Button>
-
-        <Button variant='default' size='sm'>
-          1
-        </Button>
-        <Button variant='outline' size='sm'>
-          2
-        </Button>
-        <Button variant='outline' size='sm'>
-          3
-        </Button>
-        <Button variant='ghost' size='sm' disabled>
-          <MoreHorizontal className='h-4 w-4' />
-        </Button>
-
-        <Button variant='outline' size='sm'>
-          Next
-          <ChevronRight className='h-4 w-4' />
-        </Button>
-      </div>
+      <CustomPagination totalPages={8} />
     </>
   )
 }
