9ee757d5661ca6559744dc8c7cebfa52087cbffd
dieginin <diegobalestra9@gmail.com>
2025-11-26 10:23:57 -0800

    feat: add unit tests for FavoritesContext to validate favorite toggling and localStorage interactions

---
+++ 05-heroes-app/src/context/Favorites.context2.test.tsx
@@ -0,0 +1,96 @@
+import { FavoritesContext, FavoritesProvider } from "./Favorites.context"
+import { beforeEach, describe, expect, test, vi } from "vitest"
+import { fireEvent, render, screen } from "@testing-library/react"
+
+import type { Hero } from "@/heroes/interfaces/hero.interface"
+import { use } from "react"
+
+const mockHero = {
+  id: "1",
+  name: "Batman",
+} as Hero
+
+const localStorageMock = {
+  getItem: vi.fn(),
+  setItem: vi.fn(),
+  clear: vi.fn(),
+}
+Object.defineProperty(window, "localStorage", { value: localStorageMock })
+
+const TestComponent = () => {
+  const { favorites, favoritesCount, isFavorite, toggleFavorite } =
+    use(FavoritesContext)
+
+  return (
+    <div>
+      <div data-testid='favorite-count'>{favoritesCount}</div>
+
+      <div data-testid='favorite-list'>
+        {favorites.map((favorite) => (
+          <div key={favorite.id} data-testid={`hero-${favorite.id}`}>
+            {favorite.name}
+          </div>
+        ))}
+      </div>
+
+      <button
+        data-testid='toggle-favorite'
+        onClick={() => toggleFavorite(mockHero)}
+      >
+        Toggle Favorite
+      </button>
+      <div data-testid='is-favorite'>{isFavorite(mockHero).toString()}</div>
+    </div>
+  )
+}
+
+const renderContextTest = () =>
+  render(
+    <FavoritesProvider>
+      <TestComponent />
+    </FavoritesProvider>
+  )
+
+describe("FavoritesContext", () => {
+  beforeEach(() => vi.clearAllMocks())
+
+  test("should initialize with initial values", () => {
+    renderContextTest()
+
+    expect(screen.getByTestId("favorite-count").textContent).toBe("0")
+    expect(screen.getByTestId("favorite-list").children.length).toBe(0)
+  })
+
+  test("should add hero to favorites when toggleFavorite is called with new hero", () => {
+    renderContextTest()
+
+    const button = screen.getByTestId("toggle-favorite")
+    fireEvent.click(button)
+
+    expect(screen.getByTestId("favorite-count").textContent).toBe("1")
+    expect(screen.getByTestId("is-favorite").textContent).toBe("true")
+    expect(screen.getByTestId("hero-1").textContent).toBe("Batman")
+    expect(localStorageMock.setItem).toHaveBeenCalled()
+    expect(localStorageMock.setItem).toHaveBeenCalledWith(
+      "favorites",
+      '[{"id":"1","name":"Batman"}]'
+    )
+  })
+
+  test("should remove hero to favorites when toggleFavorite is called", () => {
+    localStorageMock.getItem.mockReturnValue(JSON.stringify([mockHero]))
+    renderContextTest()
+
+    const button = screen.getByTestId("toggle-favorite")
+    fireEvent.click(button)
+
+    expect(screen.getByTestId("favorite-count").textContent).toBe("0")
+    expect(screen.getByTestId("is-favorite").textContent).toBe("false")
+    expect(screen.queryByTestId("hero-1")).toBeNull()
+    expect(localStorageMock.setItem).toHaveBeenCalled()
+    expect(localStorageMock.setItem).toHaveBeenCalledWith(
+      "favorites",
+      '[{"id":"1","name":"Batman"}]'
+    )
+  })
+})
