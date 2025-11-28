1f68ad6d8b42dc362918ab8e94e8608bc13e5f52
dieginin <diegobalestra9@gmail.com>
2025-11-28 13:04:55 -0800

    feat: update Header and GenderPage components for improved navigation and display

--- 07-teslo-shop/src/shop/pages/gender/GenderPage.tsx
+++ 07-teslo-shop/src/shop/pages/gender/GenderPage.tsx
@@ -1,3 +1,20 @@
+import { Jumbotron } from "@/shop/components/Jumbotron"
+import { Pagination } from "@/components/shared/Pagination"
+import { ProductsGrid } from "@/shop/components/ProductsGrid"
+import { products } from "@/mocks/products.mock"
+import { useParams } from "react-router"
+
 export const GenderPage = () => {
-  return <div>GenderPage</div>
+  const { gender } = useParams()
+
+  const genderLabel =
+    gender === "men" ? "Hombres" : gender === "women" ? "Mujeres" : "Niños"
+
+  return (
+    <>
+      <Jumbotron title={`Productos para ${genderLabel}`} />
+      <ProductsGrid products={products} />
+      <Pagination totalPages={5} />
+    </>
+  )
 }
