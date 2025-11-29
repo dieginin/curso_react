395d88d07c3237726e416d5dca3b16384ed2c7fb
dieginin <diegobalestra9@gmail.com>
2025-11-28 17:35:02 -0800

    feat: add admin dashboard components including ActivityFeed, Chart, QuickActions, StatCard, and Title

--- 07-teslo-shop/src/admin/layouts/AdminLayout.tsx
+++ 07-teslo-shop/src/admin/layouts/AdminLayout.tsx
@@ -1,10 +1,22 @@
+import { Header } from "../components/Header"
 import { Outlet } from "react-router"
+import { Sidebar } from "../components/Sidebar"
+import { useState } from "react"
 
 const AdminLayout = () => {
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false)
+
+  const toggleSidebar = () => setSidebarCollapsed(!sidebarCollapsed)
+
   return (
-    <div>
-      AdminLayout
-      <Outlet />
+    <div className='min-h-screen bg-gray-50 flex'>
+      <Sidebar isCollapsed={sidebarCollapsed} onToggle={toggleSidebar} />
+
+      <div className='flex-1 flex flex-col'>
+        <Header />
+
+        <main className='flex-1 p-6' children={<Outlet />} />
+      </div>
     </div>
   )
 }
