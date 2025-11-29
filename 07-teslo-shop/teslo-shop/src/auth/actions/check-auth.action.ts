95c211bc3e529efa08608600c7bb3333b7adb798
dieginin <diegobalestra9@gmail.com>
2025-11-29 15:55:21 -0800

    feat: add request interceptor to attach authorization token and implement checkAuthAction for token validation

---
+++ 07-teslo-shop/teslo-shop/src/auth/actions/check-auth.action.ts
@@ -0,0 +1,18 @@
+import type { AuthResponse } from "../interfaces/auth.response"
+import { tesloApi } from "@/api/teslo.api"
+
+export const checkAuthAction = async (): Promise<AuthResponse> => {
+  const token = localStorage.getItem("token")
+  if (!token) throw new Error("No token found")
+
+  try {
+    const { data } = await tesloApi.get<AuthResponse>("/auth/check-status")
+
+    localStorage.setItem("token", data.token)
+
+    return data
+  } catch {
+    localStorage.removeItem("token")
+    throw new Error("Token expired or not valid")
+  }
+}
