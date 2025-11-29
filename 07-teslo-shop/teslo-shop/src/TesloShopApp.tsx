3d060ba7e8d820031616db1b6c4962250ee8ecdd
dieginin <diegobalestra9@gmail.com>
2025-11-29 15:33:42 -0800

    fix: correct syntax error in TesloShopApp component rendering

--- 07-teslo-shop/teslo-shop/src/TesloShopApp.tsx
+++ 07-teslo-shop/teslo-shop/src/TesloShopApp.tsx
@@ -11,7 +11,7 @@ export const TesloShopApp = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster />
-      <RouterProvider router={appRouter} />)
+      <RouterProvider router={appRouter} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
