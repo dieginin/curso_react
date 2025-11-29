a2e67563ccaebd04e2f2d065946d938f658b2183
dieginin <diegobalestra9@gmail.com>
2025-11-29 00:19:33 -0800

    feat: enhance getProducts function to accept pagination options; update useProducts hook accordingly

--- 07-teslo-shop/teslo-shop/src/shop/actions/get-products.action.ts
+++ 07-teslo-shop/teslo-shop/src/shop/actions/get-products.action.ts
@@ -4,8 +4,19 @@ import { tesloApi } from "@/api/teslo.api"
 const transformImageUrl = (image: string) =>
   `${import.meta.env.VITE_API_URL}/files/product/${image}`
 
-export const getProducts = async (): Promise<ProductsResponse> => {
-  const { data } = await tesloApi.get<ProductsResponse>("/products")
+interface Options {
+  limit?: number | string
+  offset?: number | string
+}
+
+export const getProducts = async (
+  options: Options
+): Promise<ProductsResponse> => {
+  const { limit, offset } = options
+
+  const { data } = await tesloApi.get<ProductsResponse>("/products", {
+    params: { limit, offset },
+  })
 
   const updatedProducts = data.products.map((product) => ({
     ...product,
--- 07-teslo-shop/teslo-shop/src/shop/hooks/useProducts.ts
+++ 07-teslo-shop/teslo-shop/src/shop/hooks/useProducts.ts
@@ -1,11 +1,21 @@
 import { getProducts } from "@/shop/actions/get-products.action"
 import { useQuery } from "@tanstack/react-query"
+import { useSearchParams } from "react-router"
 
 export const useProducts = () => {
-  // TODO viene lógica
+  const [searchParams] = useSearchParams()
+
+  const limit = searchParams.get("limit") || 9
+  const page = searchParams.get("page") || 1
+  const offset = (+page - 1) * +limit
 
   return useQuery({
-    queryKey: ["products"],
-    queryFn: getProducts,
+    queryKey: ["products", { limit, offset }],
+    queryFn: () =>
+      getProducts({
+        limit: isNaN(+limit) ? 9 : limit,
+        offset: isNaN(offset) ? 0 : offset,
+      }),
+    staleTime: 1000 * 60 * 5,
   })
 }
