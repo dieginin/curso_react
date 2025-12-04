65c644901fed38f8d79316fe9d3de468884d2bb8
dieginin <diegobalestra9@gmail.com>
2025-12-03 21:57:00 -0800

    feat: add Calendar component with event handling and styling

--- 08-calendar/src/app/pages/CalendarPage.jsx
+++ 08-calendar/src/app/pages/CalendarPage.jsx
@@ -1,9 +1,43 @@
+import "react-big-calendar/lib/css/react-big-calendar.css"
+
+import { Calendar, dateFnsLocalizer } from "react-big-calendar"
+import { addHours, format, getDay, parse, startOfWeek } from "date-fns"
+
 import { Navbar } from "../components/Navbar"
+import { enUS } from "date-fns/locale"
+
+const locales = {
+  "en-US": enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [
+  {
+    title: "Cumpleaños del jefe",
+    notas: "Hay que comprar el pastel",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+  },
+]
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor='start'
+        endAccessor='end'
+        style={{ height: "calc(100vh - 56px)" }}
+      />
     </>
   )
 }
