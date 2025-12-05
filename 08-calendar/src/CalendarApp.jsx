88d49527a569f4a57797a7964d646f01a6900f92
dieginin <diegobalestra9@gmail.com>
2025-12-04 22:35:56 -0800

    feat: integrate Redux for state management and add UI slice for modal control

--- 08-calendar/src/CalendarApp.jsx
+++ 08-calendar/src/CalendarApp.jsx
@@ -1,10 +1,14 @@
 import { AppRouter } from "./app.router"
 import { BrowserRouter } from "react-router"
+import { Provider } from "react-redux"
+import { store } from "./store"
 
 export const CalendarApp = () => {
   return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </Provider>
   )
 }
