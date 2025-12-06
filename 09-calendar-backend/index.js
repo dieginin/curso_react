164992c37f5b3829962e0020d026b9fbffafce0e
dieginin <diegobalestra9@gmail.com>
2025-12-05 22:05:15 -0800

    Add express as a dependency in package.json

--- 09-calendar-backend/index.js
+++ 09-calendar-backend/index.js
@@ -1,1 +1,14 @@
-console.log("Hola mundo!!")
+const express = require("express")
+
+// * Crear servidor
+const app = express()
+
+// * Rutas
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,
+  })
+})
+
+// * Escuchar peticiones
+app.listen(4995, () => console.log(`Servidor corriendo en puerto ${4995}`))
