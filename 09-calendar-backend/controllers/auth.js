e09c4948384076fcb1c40165a9a52e9c5c397746
dieginin <diegobalestra9@gmail.com>
2025-12-05 22:49:03 -0800

    feat: add authentication controller and routes for user management

---
+++ 09-calendar-backend/controllers/auth.js
@@ -0,0 +1,28 @@
+const { request, response } = require("express")
+
+const crearUsuario = (req = request, res = response) => {
+  res.json({
+    ok: true,
+    msg: "login",
+  })
+}
+
+const loginUsuario = (req = request, res = response) => {
+  res.json({
+    ok: true,
+    msg: "registro",
+  })
+}
+
+const renovarToken = (req = request, res = response) => {
+  res.json({
+    ok: true,
+    msg: "renew",
+  })
+}
+
+module.exports = {
+  crearUsuario,
+  loginUsuario,
+  renovarToken,
+}
--- 09-calendar-backend/routes/auth.js
+++ 09-calendar-backend/routes/auth.js
@@ -6,10 +6,16 @@
 const { Router } = require("express")
 const router = Router()
 
-router.get("/", (req, res) => {
-  res.json({
-    ok: true,
-  })
-})
+const {
+  crearUsuario,
+  loginUsuario,
+  renovarToken,
+} = require("../controllers/auth")
+
+router.post("/", crearUsuario)
+
+router.post("/new", loginUsuario)
+
+router.get("/renew", renovarToken)
 
 module.exports = router
