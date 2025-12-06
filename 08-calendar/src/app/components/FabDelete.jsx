55b27ee71e9e3d37e244cdfdac032f68ec21325f
dieginin <diegobalestra9@gmail.com>
2025-12-05 21:12:01 -0800

    feat: update FabDelete component to prevent deletion when date modal is open

--- 08-calendar/src/app/components/FabDelete.jsx
+++ 08-calendar/src/app/components/FabDelete.jsx
@@ -1,13 +1,14 @@
-import { useCalendarStore } from "../../hooks"
+import { useCalendarStore, useUiStore } from "../../hooks"
 
 export const FabDelete = () => {
   const { hasEventSelected, startDeletingEvent } = useCalendarStore()
+  const { isDateModalOpen } = useUiStore()
 
   const handleDelete = async () => startDeletingEvent()
 
   return (
     <>
-      {hasEventSelected && (
+      {hasEventSelected & !isDateModalOpen && (
         <button className={`btn btn-danger fd`} onClick={handleDelete}>
           <i className='fas fa-trash' />
         </button>
