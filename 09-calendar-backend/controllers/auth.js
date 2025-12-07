b918b4996e2c7b10285a0b66fc68ab435e797291
dieginin <diegobalestra9@gmail.com>
2025-12-06 19:57:35 -0800

    feat: add JWT generation for user authentication and update dependencies

--- 09-calendar-backend/controllers/auth.js
+++ 09-calendar-backend/controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require("express")
 const bcrypt = require("bcryptjs")
 const Usuario = require("../models/Usuario")
+const { generarJWT } = require("../helpers/jwt")
 
 const crearUsuario = async (req, res = response) => {
   const { email, password } = req.body
@@ -22,10 +23,14 @@ const crearUsuario = async (req, res = response) => {
 
     await usuario.save()
 
+    // Generar JWT
+    const token = await generarJWT(usuario.id, usuario.name)
+
     res.status(201).json({
       ok: true,
       uid: usuario.id,
       name: usuario.name,
+      token,
     })
   } catch (error) {
     console.log(error)
@@ -58,11 +63,13 @@ const loginUsuario = async (req, res = response) => {
     }
 
     // Generar JWT
+    const token = await generarJWT(usuario.id, usuario.name)
 
     res.status(201).json({
       ok: true,
       uid: usuario.id,
       name: usuario.name,
+      token,
     })
   } catch (error) {
     console.log(error)
