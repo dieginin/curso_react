63c529463724c73b333416945fcdb8ee8b3214d8
dieginin <diegobalestra9@gmail.com>
2025-12-07 11:44:04 -0800

    feat: add authSlice for user authentication management

--- 08-calendar/src/store/store.js
+++ 08-calendar/src/store/store.js
@@ -1,9 +1,10 @@
-import { calendarSlice, uiSlice } from "."
+import { authSlice, calendarSlice, uiSlice } from "."
 
 import { configureStore } from "@reduxjs/toolkit"
 
 export const store = configureStore({
   reducer: {
+    auth: authSlice.reducer,
     ui: uiSlice.reducer,
     calendar: calendarSlice.reducer,
   },
