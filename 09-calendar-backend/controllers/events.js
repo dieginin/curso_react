4e4ff94629e8417b7c1a98b68148698ef5f036c9
dieginin <diegobalestra9@gmail.com>
2025-12-06 22:52:44 -0800

    feat: implement event creation with user association and error handling

--- 09-calendar-backend/controllers/events.js
+++ 09-calendar-backend/controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require("express")
+const Evento = require("../models/Evento")
 
 const actualizarEvento = (req, res = response) => {
   const { id } = req.params
@@ -10,11 +11,23 @@ const actualizarEvento = (req, res = response) => {
   })
 }
 
-const crearEvento = (req, res = response) => {
-  res.status(200).json({
-    ok: true,
-    msg: "Crear evento",
-  })
+const crearEvento = async (req, res = response) => {
+  const newEvent = new Evento(req.body)
+
+  try {
+    newEvent.user = req.uid
+    const evento = await newEvent.save()
+
+    res.status(200).json({
+      ok: true,
+      evento,
+    })
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor contacte al administrador",
+    })
+  }
 }
 
 const eliminarEvento = (req, res = response) => {
