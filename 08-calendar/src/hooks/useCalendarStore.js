5d04f6394d650cbcb52601ec23a5957c3489fd23
dieginin <diegobalestra9@gmail.com>
2025-12-07 23:37:08 -0800

    feat: enhance event creation logic in useCalendarStore with user information

--- 08-calendar/src/hooks/useCalendarStore.js
+++ 08-calendar/src/hooks/useCalendarStore.js
@@ -6,23 +6,27 @@ import {
 } from "../store"
 import { useDispatch, useSelector } from "react-redux"
 
+import { calendarApi } from "../api"
+
 export const useCalendarStore = () => {
   const dispatch = useDispatch()
 
   const { activeEvent, events } = useSelector((state) => state.calendar)
+  const { user } = useSelector((state) => state.auth)
 
   const setActiveEvent = (calendarEvent) =>
     dispatch(onSetActiveEvent(calendarEvent))
 
   const startSavingEvent = async (calendarEvent) => {
-    // TODO: llegar al backend
-    // Todo bien
+    // TODO: update event
     if (calendarEvent._id) {
       //* ACTUALIZANDO
       dispatch(onUpdateEvent(calendarEvent))
     } else {
       //* CREANDO
-      dispatch(onAddNewEvent({ ...calendarEvent, _id: new Date().getTime }))
+      const { data } = await calendarApi.post("/events", calendarEvent)
+
+      dispatch(onAddNewEvent({ ...calendarEvent, id: data.evento.id, user }))
     }
   }
 
